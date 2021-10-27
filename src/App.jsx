f9580dd55c39ebc881bae9301d74e6a2eedac5ab
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-10-27 13:37:09 -0400

    Created Checkout, AddressForm, PaymentForm, CustomTextField, and Review

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Products, Navbar, Cart } from './components'
+import { Products, Navbar, Cart, Checkout } from './components'
 import { commerce } from './lib/commerce';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
@@ -47,12 +47,15 @@ const App = () => {
                     <Route exact path = '/'>
                         <Products products={products} onAddToCart={handleAddToCart} />
                     </Route>
-                    <Route exact path = '/cart/'>
+                    <Route exact path = '/cart'>
                         <Cart cart = {cart}
                             handleEmptyCart = {handleEmptyCart}
                             handleRemoveFromCart = {handleRemoveFromCart}
                             handleUpdateCartQty = {handleUpdateCartQty}
                         />
+                    </Route>
+                    <Route exact path ='/checkout'>
+                        <Checkout />
                     </Route>
                 </Switch>
             </div>
