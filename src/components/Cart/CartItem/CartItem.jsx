115fdee82a22df995ff9b75458f2c4c4ce7fd598
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-10-27 12:55:02 -0400

    E-Commerce front end functional; update quantity and remove from cart functional

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@material-ui/core';
 import useStyles from './cartItemStyles.js'
 
-const CartItem = ({ item }) => {
+const CartItem = ({ item, handleUpdateCartQty, handleRemoveFromCart }) => {
     const classes = useStyles();
     return (
         <Card>
@@ -13,11 +13,11 @@ const CartItem = ({ item }) => {
             </CardContent>
             <CardActions className = {classes.cardActions}>
                 <div className = {classes.buttons}>
-                    <Button type = 'button' size = 'small'>-</Button>
+                    <Button type = 'button' size = 'small' onClick = {() => handleUpdateCartQty(item.id, item.quantity - 1)}>-</Button>
                     <Typography>{item.quantity}</Typography>
-                    <Button type = 'button' size = 'small'>+</Button>
+                    <Button type = 'button' size = 'small' onClick = {() => handleUpdateCartQty(item.id, item.quantity + 1)}>+</Button>
                 </div>
-                <Button variant = 'contained' type = 'button' color='secondary'>Remove Item</Button>
+                <Button variant = 'contained' type = 'button' color='secondary' onClick = {() => handleRemoveFromCart(item.id)}>Remove Item</Button>
             </CardActions>
         </Card>
  
