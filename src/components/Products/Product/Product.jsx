039d9fe49bb6bee570564e32f000d2ed6f533e10
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-10-27 10:09:36 -0400

    Laid out basic product grid; Seeded test products

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
-import React from 'react'
+import useStyles from './productStyles'
 
 const Product = ( { product }) => {
+    const classes = useStyles();
     return (
         <Card className={classes.root}>
-            <CardMedia className={classes.media} image='' title={product.name} />
+            <CardMedia className={classes.media} image={product.image} title={product.name} />
             <CardContent>
                 <div className = {classes.cardContent}>
                     <Typography variant='h5' gutterBottom>
