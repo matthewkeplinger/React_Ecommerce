400f644704e0701e2f1e07b86ea67b1a83ae747e
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-10-27 12:08:52 -0400

    Added shopping cart; basic functionality tested

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Products, Navbar } from './components'
+import { Products, Navbar, Cart } from './components'
 import { commerce } from './lib/commerce';
 
 const App = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState({});
 
     const fetchProducts = async () => {
         const { data } = await commerce.products.list()
@@ -11,15 +12,26 @@ const App = () => {
         setProducts(data);
     }
 
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve())
+    }
+
+    const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity);
+        setCart(item.cart);
+    }
+
     useEffect(() => {
         fetchProducts();
+        fetchCart();
     }, []);
 
-    console.log(products)
+    console.log(cart)
     return (
         <div>
-            <Navbar />
-            <Products products={products} />
+            <Navbar totalItems = {cart.total_items}/>
+            {/* <Products products={products} onAddToCart={handleAddToCart} /> */}
+            <Cart cart = {cart} />
         </div>
     )
 }
